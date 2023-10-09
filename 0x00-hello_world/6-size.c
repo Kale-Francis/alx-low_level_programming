a69e0b543053972f8c700351a47a0afc308f1eb5
king-vader <kalefrancis989@gmail.com>
2023-10-08 18:08:16 -0700

    Hello World

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char:%d byte(s)\n",sizeof(char));
+	printf("Size of an int: %d byte(s)\n",sizeof(intl));
+	printf("size of a long intl:%d byte(s)\n",sizeof(long intl));
+	printf("size of a long long intl:%d byte(s)\n",sizeof(long long intl));
+	printf("size of a float:%d byte(s)\n",sizeof(float));
+	return(0);
+}
