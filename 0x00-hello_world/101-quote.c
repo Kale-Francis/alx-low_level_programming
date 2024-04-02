9421d83f5f84a65f27f6349f938848a95195c615
king-vader <kalefrancis989@gmail.com>
2024-04-02 08:22:04 -0700

    Matrix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,10 @@
-#include<stdio.h>
-#include<unsitd.h>
+#!/bin/bash
+#include <stdio.h>
+#include <unistd.h>
 
-/**
- * main -Entry point
- *
- * Return: Always 1 (Success)
- */
-int main(void)
-{
-	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59)
-	return(1)
+int main(void) {
+    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(STDERR_FILENO, message, sizeof(message) - 1);
+    return 1;
 }
+
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,3 +1,4 @@
+#!/bin/bash
 #include<stdio.h>
 /**
  * main -Entry point
@@ -6,10 +7,12 @@
  */
 int main(void)
 {
-	printf("Size of a char:%d byte(s)\n",sizeof(char));
-	printf("Size of an int: %d byte(s)\n",sizeof(intl));
-	printf("size of a long intl:%d byte(s)\n",sizeof(long intl));
-	printf("size of a long long intl:%d byte(s)\n",sizeof(long long intl));
-	printf("size of a float:%d byte(s)\n",sizeof(float));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+    printf("Size of an int: %zu byte(s)\n", sizeof(int));
+    printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+    printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+    printf("Size of a float: %zu byte(s)\n", sizeof(float));
+    printf("Size of a double: %zu byte(s)\n", sizeof(double));
+    printf("Size of a long double: %zu byte(s)\n", sizeof(long double));
 	return(0);
 }
